fee5396cf26a78885cbf13296df1db3098096387
anshu <anshu.ac.dv@gmail.com>
2026-05-11 20:15:05 +0530

    Initial Commit

---
+++ lib/Bloc/Counter/counter_event.dart
@@ -0,0 +1,21 @@
+import 'package:equatable/equatable.dart';
+
+abstract class CounterEvent extends Equatable {
+  const CounterEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class CounterIncrementEvent extends CounterEvent {
+  const CounterIncrementEvent();
+}
+
+class CounterDecrementEvent extends CounterEvent {
+  const CounterDecrementEvent();
+}
+
+class CounterResetEvent extends CounterEvent {
+  const CounterResetEvent();
+}
+

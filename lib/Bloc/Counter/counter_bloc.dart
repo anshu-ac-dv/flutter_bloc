e98eab345ce8e1853bc0b183ce79aa442dd3ddf7
anshu <anshu.ac.dv@gmail.com>
2026-05-11 20:37:29 +0530

    Initial Commit

---
+++ lib/Bloc/Counter/counter_bloc.dart
@@ -0,0 +1,19 @@
+import 'package:bloc/bloc.dart';
+import 'package:bloc_learning/Bloc/Counter/counter_event.dart';
+import 'package:bloc_learning/Bloc/Counter/counter_state.dart';
+
+class CounterBloc extends Bloc<CounterEvent, CounterState>{
+  CounterBloc() :super(CounterState()){
+    on<CounterIncrementEvent>((event, emit) {
+      emit(state.copyWith(counter: state.counter + 1));
+    });
+
+    on<CounterDecrementEvent>((event, emit) {
+      emit(state.copyWith(counter: state.counter - 1));
+    });
+
+    on<CounterResetEvent>((event, emit) {
+      emit(state.copyWith(counter: 0));
+    });
+  }
+}

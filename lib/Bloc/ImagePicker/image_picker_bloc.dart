b47e176bf847838334e604c92a82867eac88f6db
anshu <anshu.ac.dv@gmail.com>
2026-05-14 20:17:44 +0530

    Initial Commit

--- lib/Bloc/ImagePicker/image_picker_bloc.dart
+++ lib/Bloc/ImagePicker/image_picker_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:bloc_learning/Utils/image_picker_utils.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
 import 'package:image_picker/image_picker.dart';
 
 part 'image_picker_event.dart';
@@ -14,12 +15,28 @@ class ImagePickerBloc extends Bloc<ImagePickerEvent, ImagePickerState> {
   }
 
   Future<void> _cameraCapture(CameraCapture event, Emitter<ImagePickerState> emit) async {
-    XFile? file = await imagePickerUtils.cameraCapture();
-    emit(state.copyWith(file: file));
+    try {
+      XFile? file = await imagePickerUtils.cameraCapture();
+      if (file != null) {
+        emit(state.copyWith(file: file));
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print("Camera Error: $e");
+      }
+    }
   }
 
   Future<void> _galleryPicker(GalleryPicker event, Emitter<ImagePickerState> emit) async {
-    XFile? file = await imagePickerUtils.galleryPicker();
-    emit(state.copyWith(file: file));
+    try {
+      XFile? file = await imagePickerUtils.galleryPicker();
+      if (file != null) {
+        emit(state.copyWith(file: file));
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print("Gallery Error: $e");
+      }
+    }
   }
 }

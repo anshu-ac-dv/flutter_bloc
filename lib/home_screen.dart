ac2b68b6fd23fea1ea7d1310b1bfc32375fb53aa
anshu <anshu.ac.dv@gmail.com>
2026-05-16 16:29:30 +0530

    Initial Commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'Counter/counter_screen.dart';
+import 'ImagePicker/image_picker.dart';
+import 'Switch/switch_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,8 +16,57 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
       ),
-      body: const Center(child: Text('Home Screen')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Center(
+            child: Text(
+              'Welcome to BLoC Learning',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const CounterScreen(),
+                ),
+              );
+            },
+            child: const Text('Counter Example'),
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SwitchScreen(),
+                ),
+              );
+            },
+            child: const Text('Switch Example'),
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const ImagePickerScreen(),
+                ),
+              );
+            },
+            child: const Text('Image Picker Example'),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
-import 'package:bloc_learning/ImagePicker/image_picker.dart';
-import 'package:bloc_learning/Switch/switch_screen.dart';
 import 'package:bloc_learning/Utils/image_picker_utils.dart';
+import 'package:bloc_learning/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'Bloc/Counter/counter_bloc.dart';
 import 'Bloc/ImagePicker/image_picker_bloc.dart';
 import 'Bloc/Switch/switch_bloc.dart';
-import 'Counter/counter_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,9 +28,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
-        //home: const CounterScreen(),
-        //home: SwitchScreen(),
-        home: const ImagePickerScreen(),
+        home: const HomeScreen(),
       ),
     );
   }
